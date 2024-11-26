285a7ea078528b384e42f6e18cb34bc4efc07180
bene <bensatriya3@gmail.com>
2024-11-26 23:15:54 +0700

    init

---
+++ src/main/java/com/example/amikompedia/AmikompediaApplication.java
@@ -0,0 +1,13 @@
+package com.example.amikompedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmikompediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmikompediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/amikompedia/AmikompediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amikompedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmikompediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
